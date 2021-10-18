7516d19133b8a176af929369e9c8f97a03cee590
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 16:28:55 +0200

    Create exercise-02.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-02/exercise-02.py
@@ -0,0 +1,7 @@
+# Write a program to fill the screen horizontally and vertically with your name. [Hint: add the
+# option end= '' into the print function to fill the screen horizontally.]
+
+for i in range(100):
+    for j in range(100):
+        print('AhmadAbdulrahman', end='')
+    print('')  # to start a new line
