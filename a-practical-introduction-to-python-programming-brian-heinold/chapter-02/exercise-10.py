89521347bb0b1a0feed643859644aee8b9b8d849
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 17:16:13 +0200

    Create exercise-10.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-02/exercise-10.py
@@ -0,0 +1,11 @@
+# Use a for loop to print a box like the one below. Allow the user to specify how wide and how
+# high the box should be. [Hint: print('*' * 10) prints ten asterisks.]
+
+# How wide should the box be? 7
+# How high should the box be? 10
+
+wide = eval(input('How wide should the box be? '))
+high = eval(input('How high should the box be? '))
+
+for i in range(high):
+    print('*' * wide)
