fec147119c23f9d32af2f6a9da21266ba00361bb
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 16:51:09 +0200

    Create exercise-07.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-02/exercise-07.py
@@ -0,0 +1,21 @@
+# Write a program that uses exactly four for loops to print the sequence of letters below.
+
+# AAAAAAAAAABBBBBBBCDCDCDCDEFFFFFFG
+
+for i in range(10):
+    print('A', end='')
+
+for i in range(7):
+    print('B', end='')
+
+for i in range(4):
+    print('CD', end='')
+
+print('E', end='')
+
+for i in range(6):
+    print('F', end='')
+
+print('G')
+
+# output: AAAAAAAAAABBBBBBBCDCDCDCDEFFFFFFG
