0448b0e42498fc48f18ea1990c446b04cc6f7fa0
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 15:38:14 +0200

    Create exercise-07.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-01/exercise-07.py
@@ -0,0 +1,10 @@
+# Write a program that asks the user for a weight in kilograms and converts it to pounds. There
+# are 2.2 pounds in a kilogram.
+
+# Enter a weight in kilograms: 7
+# The weight in pounds is: 15.4
+
+kilograms = eval(input('Enter a weight in kilograms: '))
+pounds = kilograms * 2.2
+
+print('The weight in pounds is:', pounds)
