614b90b2d715dd8a4a3c6294fc4ccd019b73e984
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 15:13:09 +0200

    Create exercise-03.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-01/exercise-03.py
@@ -0,0 +1,12 @@
+# Print a triangle like the one below.
+#       *
+#       **
+#       ***
+#       ****
+
+print('*')
+print('**')
+print('***')
+print('****')
+
+# note that you can make a better program using loops, but it is not illustrated yet
