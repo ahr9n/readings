79849144c49463ed903a7c6869b7e29d6eddbf38
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 16:31:17 +0200

    Create exercise-03.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-02/exercise-03.py
@@ -0,0 +1,12 @@
+# Write a program that outputs 100 lines, numbered 1 to 100, each with your name on it. The
+# output should look like the output below.
+
+# 1 Your name
+# 2 Your name
+# 3 Your name
+# 4 Your name
+# ...
+# 100 Your name
+
+for i in range(100):
+    print(i + 1, 'Ahmad Abdulrahman')
