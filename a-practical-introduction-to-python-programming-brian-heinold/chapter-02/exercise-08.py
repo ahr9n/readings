7760a80dd549893941302e594b76314c79d97371
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-18 16:56:15 +0200

    Create exercise-08.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-02/exercise-08.py
@@ -0,0 +1,11 @@
+# Write a program that asks the user for their name and how many times to print it. The pro-
+# gram should print out the user’s name the specified number of times.
+
+# Enter your name: Ahmad Abdulrahman
+# How many times do you need to print your name? 10
+
+name = input('Enter your name: ')
+times = eval(input('How many times do you need to print your name? '))
+
+for i in range(times):
+    print(name)
