2bac99ed91bba1df764f2076717e96ddb21c1867
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-21 18:30:56 +0200

    Create exercise-02.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-03/exercise-02.py
@@ -0,0 +1,12 @@
+# 2. Write a program that generates a random number, x, between 1 and 50, a random number y
+# between 2 and 5, and computes pow(x, y).
+
+from random import randint
+
+x = randint(1, 50)  # base
+y = randint(2, 5)  # exp or power
+z = x**y  # same as pow(x, y)
+
+print('Base:', x)
+print('Power:', y)
+print('Result:', z)
