6f017061438e568fd8b02f7342c894478198c3e0
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-27 00:31:56 +0200

    Create exercise-08.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-03/exercise-08.py
@@ -0,0 +1,10 @@
+# 8. Write a program that asks the user for a number of seconds and prints out how many minutes
+# and seconds that is. For instance, 200 seconds is 3 minutes and 20 seconds. [Hint: Use the //
+# operator to get minutes and the % operator to get seconds.]
+
+seconds = eval(input('Enter a number of seconds: '))
+
+minutes = seconds // 60
+seconds %= 60
+
+print('That means', minutes, 'minutes and', seconds, 'seconds.')
