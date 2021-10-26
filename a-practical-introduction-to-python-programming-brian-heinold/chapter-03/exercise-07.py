a0adce53b259309ebe40988fae7684f9a84e98db
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-27 00:27:13 +0200

    Create exercise-07.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-03/exercise-07.py
@@ -0,0 +1,10 @@
+# 7. Write a program that asks the user to enter an angle between −180◦ and 180◦. Using an
+# expression with the modulo operator, convert the angle to its equivalent between 0◦ and
+# 360◦.
+
+angle = eval(input('Enter an angle between −180◦ and 180◦: '))
+
+if angle < 0:
+    angle %= 360  # or: += 360 (aka fixing mod)
+
+print(angle)
