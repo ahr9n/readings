bb4c18ce2e68a4cd575920c62bb52b2152294883
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-27 02:01:34 +0200

    Create exercise-14.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-03/exercise-14.py
@@ -0,0 +1,16 @@
+# 14. Write a program that asks the user to enter an angle in degrees and prints out the sine of that
+# angle.
+
+import math
+
+angle_degree = eval(input('Enter an angle in degrees: '))
+
+# Note: Trigonometric functions receive the parameters only in radian
+# Now, we should convert angle from degrees to radian
+
+# radian = degree / 180 * pi
+angle_radian = angle_degree / 180 * math.pi
+
+sine = math.sin(angle_radian)
+
+print('Sine of ', angle_degree, '◦ is ', sine, sep='')
