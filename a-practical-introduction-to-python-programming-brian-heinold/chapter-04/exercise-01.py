b317215959701ea7256338e04bebd2260c07da15
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-28 14:30:54 +0200

    Create exercise-01.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-04/exercise-01.py
@@ -0,0 +1,13 @@
+# 1. Write a program that asks the user to enter a length in centimeters. If the user enters a negative
+# length, the program should tell the user that the entry is invalid. Otherwise, the program
+# should convert the length to inches and print out the result. There are 2.54 centimeters in an
+# inch.
+
+centimeters = eval(input('Enter a length in centimeters: '))
+
+if centimeters < 0:
+    print('The entry is invalid.')
+else:
+    inches = centimeters / 2.54
+    print('Length in inches is:', inches)
+    
