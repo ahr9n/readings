2250b4494a823d4bd210d50682827c97ae6b3ec8
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-10-30 13:57:13 +0200

    Create exercise-05.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-04/exercise-05.py
@@ -0,0 +1,12 @@
+# 5. Generate a random number between 1 and 10. Ask the user to guess the number and print a
+# message based on whether they get it right or not.
+
+from random import randint
+
+number = randint(1, 10)
+guess = eval(input('Guess a number between 1 and 10: '))
+
+if number == guess:
+    print('You are right!')
+else:
+    print('You are wrong.')
