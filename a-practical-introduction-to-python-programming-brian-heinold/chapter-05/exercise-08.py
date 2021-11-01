a3ea2564812dfbdc3374e551d76c7755abb01b1f
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-01 16:36:26 +0200

    Create exercise-08.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-05/exercise-08.py
@@ -0,0 +1,7 @@
+# 8. Write a program that swaps the values of three variables x,y, and z, so that x gets the value
+# of y, y gets the value of z, and z gets the value of x.
+
+x, y, z = 5, 10, 15
+x, y, z = y, z, x  # The power of Python ;)
+
+# print(x, y, z) : 10 15 5
