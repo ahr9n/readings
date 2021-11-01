820df707b20d7126f187fd0e7c73184b5c2a88e1
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-01 14:45:57 +0200

    Create exercise-03.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-05/exercise-03.py
@@ -0,0 +1,13 @@
+# 3. Write a program that asks the user to enter a value n, and then computes (1 + 1\2 + 1\3 + ... + 1\n) −
+# ln(n). The ln function is 'log' in the 'math' module.
+
+from math import log
+
+n = eval(input('Enter a value: '))
+
+answer = 0
+for i in range(1, n + 1):
+    answer += 1 / i
+answer -= log(n)
+
+print(answer)
