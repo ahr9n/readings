36ab1652be8a3cce7e1598ccc685ce531b0fddff
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-02 18:52:47 +0200

    Create exercise-06.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-06/exercise-06.py
@@ -0,0 +1,9 @@
+# 6. Write a program that asks the user to enter a string s and then converts s to lowercase, re-
+# moves all the periods and commas from s, and prints the resulting string.
+
+s = input('Enter a string: ')
+
+s = s.lower()
+s = s.replace('.', '')  # periods = points
+
+print(s)
