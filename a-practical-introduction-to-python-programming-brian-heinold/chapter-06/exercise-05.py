9df0ee86fb1665188876f3939bf7406b3c900513
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-02 18:47:27 +0200

    Create exercise-05.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-06/exercise-05.py
@@ -0,0 +1,17 @@
+# 5. Write a program that asks the user to enter a string. The program should create a new string
+# called new_string from the user’s string such that the second character is changed to an
+# asterisk and three exclamation points are attached to the end of the string. Finally, print
+# new_string. Typical output is shown below:
+# Enter your string: Qbert
+# Q*ert!!!
+
+s = input('Enter your string: ')
+
+new_string = s[0]
+if len(s) > 1:
+    new_string += '*'
+    if len(s) >= 2:
+        new_string += s[2:len(s)]
+new_string += '!!!'
+
+print(new_string)
