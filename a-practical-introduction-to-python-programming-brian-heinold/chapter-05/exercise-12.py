063cf03775c10da81a2e4d8d74717343580fcfa7
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-02 13:20:35 +0200

    Create exercise-12.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-05/exercise-12.py
@@ -0,0 +1,19 @@
+# 12. Write a program that asks the user to guess a random number between 1 and 10. If they guess
+# right, they get 10 points added to their score, and they lose 1 point for an incorrect guess. Give
+# the user five numbers to guess and print their score after all the guessing is done.
+
+from random import randint
+
+points = 0
+for i in range(10):
+    number = randint(1, 10)
+    guess = eval(input('Guess a number between 1 and 10: '))
+
+    if guess == number:
+        points += 10
+        print('You are right!')
+    else:
+        points -= 1
+        print('You are wrong.')
+
+print('Your final points:', points)
