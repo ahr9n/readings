3b2031bf5d5910cf14985e53bd657af9bfad1e39
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-02 18:05:45 +0200

    Create exercise-02.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-06/exercise-02.py
@@ -0,0 +1,8 @@
+# 2. A simple way to estimate the number of words in a string is to count the number of spaces
+# in the string. Write a program that asks the user for a string and returns an estimate of how
+# many words are in the string.
+
+s = input('Enter a string: ')
+
+words = s.count(' ') + 1
+print('Total number of words in string:', words)
