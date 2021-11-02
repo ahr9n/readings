f7b6115043c7305876b3446cf99884cb3b2aca4f
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-02 18:17:42 +0200

    Create exercise-03.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-06/exercise-03.py
@@ -0,0 +1,13 @@
+# 3. People often forget closing parentheses when entering formulas. Write a program that asks
+# the user to enter a formula and prints out whether the formula has the same number of open-
+# ing and closing parentheses.
+
+s = input('Enter a string: ')
+
+opening = s.count('(')
+closing = s.count(')')
+
+if opening == closing:
+    print('Closing parentheses in the string are equal to the opening ones.')
+else:
+    print('Closing parentheses in the string are not equal to the opening ones.')
