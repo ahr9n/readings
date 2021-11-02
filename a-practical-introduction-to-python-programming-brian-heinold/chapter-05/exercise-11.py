01bc53ec926d5081f6cd0a457d99a01f2a7acf8e
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-02 13:09:27 +0200

    Create exercise-11.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-05/exercise-11.py
@@ -0,0 +1,11 @@
+# 11. Write a program that computes the factorial of a number. The factorial, n!, of a number n is the
+# product of all the integers between 1 and n, including n. For instance, 5! = 1 · 2 · 3 · 4 · 5 = 120.
+# [Hint: Try using a multiplicative equivalent of the summing technique.]
+
+n = eval(input('Enter a number: '))
+
+factorial = 1
+for i in range(1, n + 1):
+    factorial *= i
+
+print('Factorial of', n, 'is:', factorial)
