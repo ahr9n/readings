58ad2a811ec104af498357263583bf55cca8d126
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-04 20:12:58 +0200

    Create exercise-11.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-06/exercise-11.py
@@ -0,0 +1,13 @@
+# 11. Write a program that asks the user to enter a word that contains the letter a. The program
+# should then print the following two lines: On the first line should be the part of the string up
+# to and including the the first a, and on the second line should be the rest of the string. Sample
+# output is shown below:
+
+# Enter a word: buffalo
+# buffa
+# lo
+
+word = input('Enter a word: ')
+idx = word.find('a')
+print(word[:idx + 1])
+print(word[idx + 1:])
