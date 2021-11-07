271afefaaec6d3a20178e965e4292cb320deb6e4
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-07 15:26:20 +0200

    Create exercise-08.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-07/exercise-08.py
@@ -0,0 +1,15 @@
+# 8. Write a program that asks the user for an integer and creates a list that consists of the factors
+# of that integer.
+import math
+
+integer = int(input('Please enter an integer: '))
+
+# factorize the integer == devisors 
+factors = []
+SQRT = math.ceil(math.sqrt(integer))
+for i in range(1, SQRT + 1):
+    if integer % i == 0:
+        factors.append(i)
+        if i != integer // i:
+            factors.append(integer // i)
+print(factors)
