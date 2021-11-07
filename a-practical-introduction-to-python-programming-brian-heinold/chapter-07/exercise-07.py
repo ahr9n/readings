93b0d437f702cd0344170ae226be744099429b58
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-07 15:04:11 +0200

    Create exercise-07.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-07/exercise-07.py
@@ -0,0 +1,13 @@
+# 7. Write a program that takes any two lists L and M of the same size and adds their elements
+# together to form a new list N whose elements are sums of the corresponding elements in L
+# and M. For instance, if L=[3,1,4] and M=[1,5,9], then N should equal [4,6,13].
+
+L = input('Enter a list of numbers: ').split()
+M = input('Enter another list of numbers: ').split()
+
+N = []
+for i in range(len(L)):
+    N.append(int(L[i]) + int(M[i]))
+# N = [(int(L[i]) + int(M[i])) for i in range(len(L))]
+
+print(N)
