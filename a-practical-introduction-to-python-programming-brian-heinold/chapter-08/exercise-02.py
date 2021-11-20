5667fcef2bfc0235d3dc1216f65d048c59117264
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-20 17:05:46 +0200

    Create exercise-02.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-08/exercise-02.py
@@ -0,0 +1,5 @@
+# 2. Write a program that allows the user to enter five numbers (read as strings). Create a string
+# that consists of the user’s numbers separated by plus signs. For instance, if the user enters 2,
+# 5, 11, 33, and 55, then the string should be '2+5+11+33+55'.
+
+print('+'.join(list(input('Enter five separate numbers: ').split())))
