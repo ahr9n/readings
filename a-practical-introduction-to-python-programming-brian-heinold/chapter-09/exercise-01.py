fa29af82c733b941109707fe3e6627e450a7b2bb
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-25 15:33:57 +0200

    Create exercise-01.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-09/exercise-01.py
@@ -0,0 +1,9 @@
+# 1. The code below prints the numbers from 1 to 50. Rewrite the code using a while loop to
+# accomplish the same thing.
+# for i in range(1,51):
+#   print(i)
+
+i = 1
+while i <= 50:
+    print(i)
+    i += 1
