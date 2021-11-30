7ed3413b81e1c4b6da215fcab1a67b84a5d91a8d
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-30 20:10:22 +0200

    Create exercise-07.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-08/exercise-07.py
@@ -0,0 +1,18 @@
+# 7. Write a program that estimates the average number of drawings it takes before the user’s
+# numbers are picked in a lottery that consists of correctly picking six different numbers that
+# are between 1 and 10. To do this, run a loop 1000 times that randomly generates a set of
+# user numbers and simulates drawings until the user’s numbers are drawn. Find the average
+# number of drawings needed over the 1000 times the loop runs.
+
+import random
+
+lottery_numbers = [i for i in range(1, 11)]
+
+avg = 0
+for i in range(1000):
+    user = random.randint(1, 10)
+    lott = random.choice(lottery_numbers)
+    if lott == user:
+        avg = avg + 1
+
+print('Average number of drawings:', round(1000 / avg, 4))
