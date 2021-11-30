afd49c86faaac293cf0bada5516eff8c06ba75f7
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2021-11-30 21:21:20 +0200

    Create exercise-15.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/chapter-08/exercise-15.py
@@ -0,0 +1,7 @@
+# 15. Use a list comprehension to create the list below, which consists of ones separated by increas-
+# ingly many zeroes. The last two ones in the list should be separated by ten zeroes.
+# [1,1,0,1,0,0,1,0,0,0,1,0,0,0,0,1,....]
+
+helper = [[0]*i + [1] for i in range(11)]
+L = [elem for List in helper for elem in List]
+print(L)
