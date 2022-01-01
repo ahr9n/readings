a036c741c03e6b90c98b9c014546217b1dca0077
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-01-01 20:09:34 +0200

    Create exercise-03.py

---
+++ a-practical-introduction-to-python-programming-brian-heinold/additional/gui/exercise-03.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from math import factorial
+
+root = Tk()
+root.geometry('450x150')
+
+
+def callback():
+    n = int(entry.get())
+    result.config(text='The factorial of {} is {}! = {}'.format(n, n, factorial(n)))
+
+
+label = Label(text='Enter value of integer N : ')
+label.place(x=20, y=20)
+
+entry = Entry()
+entry.place(x=190, y=20, width=225, height=25)
+
+result = Label()
+result.place(x=190, y=60)
+
+button = Button(text='Validate', command=callback)
+button.place(x=190, y=100, width=225, height=30)
+
+mainloop()
