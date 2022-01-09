ace5595fc7bcac70cc738adb62b8c005db65df73
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-01-09 22:31:19 +0200

    Create 2.4.1.a.java

---
+++ dragon-book/2.4.1.a.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class Parser {
+    static int lookahead;
+
+    public Parser() throws IOException {
+        lookahead = System.in.read();
+    }
+
+    public void stmt() throws Exception {
+        switch (lookahead) {
+            case '+': match('+'); stmt(); stmt(); break;
+            case '-': match('-'); stmt(); stmt(); break;
+            case 'a': match('a'); break;
+            default: throw new Error("syntax error");
+        }
+    }
+
+    void match(int t) throws IOException {
+        if (lookahead == t) lookahead = System.in.read();
+        else throw new Error("syntax error");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Parser parse = new Parser();
+        try {
+            parse.stmt();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
