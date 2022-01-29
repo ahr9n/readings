108dc143dca8ea11315170f7555aae0a646ed4af
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-01-30 00:05:50 +0200

    Update 1.1.cpp

--- cracking-the-coding-interview/interview-questions/chapter-01-arrays-and-strings/1.1.cpp
+++ cracking-the-coding-interview/interview-questions/chapter-01-arrays-and-strings/1.1.cpp
@@ -1,13 +1,6 @@
 #include "bits/stdc++.h"
 
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("-O2")
-
 using namespace std;
-using ll = long long;
-using ld = long double;
-
-const int N = 3E5 + 5, M = 1E6 + 10, MOD = 1E9 + 7, OO = 0x3f3f3f3f;
 
 bool IsUnique(string s) {
     map<char, bool> chars;
